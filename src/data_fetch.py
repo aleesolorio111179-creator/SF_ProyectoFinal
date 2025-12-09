f9c4ec9df77bde1e6996595bba5106da3bbf8fb9
aleesolorio111179-creator <aleesolorio111179@gmail.com>
2025-12-08 21:41:36 -0600

    Delete src directory

--- src/data_fetch.py
+++
@@ -1,6 +0,0 @@
-import yfinance as yf
-import pandas as pd
-
-def download_prices(tickers, start, end):
-    df = yf.download(tickers, start=start, end=end, auto_adjust=True)
-    return df["Adj Close"]
